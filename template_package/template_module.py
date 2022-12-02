dbe273935031d7173fa7ad0ee57ba1f78431dd7c
Todd Parsons <todd.e.parsons@googlemail.com>
2022-12-02 15:32:41 +0000

    Add some input variables to template class

--- template_package/template_module.py
+++ template_package/template_module.py
@@ -1,8 +1,9 @@
 class TemplateClass:
     clsAttrib = None
 
-    def __init__(self):
-        pass
+    def __init__(self, compulsoryAttrib, optionalAttrib=None):
+        self.compulsoryAttrib = compulsoryAttrib
+        self.optionalAttrib = optionalAttrib
 
     def someFunction(input1, input2, namedInput=None):
         return input1, input2, namedInput
